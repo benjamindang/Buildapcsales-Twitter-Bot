2c09d268dcff2331b930d959db1f96c94fd28366
Benjamin Dang <benjamin.d.dang@gmail.com>
2016-11-26 11:40:10 -0800

    Removed secrets and login info

--- reddit.py
+++ reddit.py
@@ -1,10 +1,10 @@
 import praw,tweepy,sys,time
 
 my_user_agent = "mac:RedditSaleReminder:v1.0 (by /u/bdang24)"
-my_client_id = "uFm6USAofxMHQQ"
-my_client_secret = "lFqznv_Wd6mwcvLhmFVUtkB6Vw8"
-my_username = "bdang24"
-my_password = "treegolem2"
+my_client_id = "ExampleClientID"
+my_client_secret = "ExampleClientSecret"
+my_username = "ExampleUserName"
+my_password = "ExamplePassword"
 
 reddit = praw.Reddit(user_agent=my_user_agent,
                      client_id=my_client_id,
@@ -12,10 +12,10 @@ reddit = praw.Reddit(user_agent=my_user_agent,
                      username=my_username,
                      password=my_password)
 
-consumer_key = "oXStSpBeEUS3ayaVay8IxSqzC"
-consumer_secret = "clRKjABnCvmVHaf9iul9CJ2ka9zCwFaLBLTcwPqx5PBVecMWcd"
-access_key = "802490163578318848-kO2FiftQ1VGX3azus58IFpTSQCIYi7e"
-access_secret = "ABJuzgKYoLfiO4tri3tXC4lkcaKCfzFScATYUEzoDm3tX"
+consumer_key = "ExampleConsumerKey"
+consumer_secret = "ExampleConsumerSecret"
+access_key = "ExampleAccessKey"
+access_secret = "ExampleAccessKey"
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 api = tweepy.API(auth)
