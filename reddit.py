609f9fdedeea97e6cad8da888a460eb1de26ee57
Benjamin Dang <benjamin.d.dang@gmail.com>
2016-11-26 11:34:32 -0800

    Included new twitter changes

--- reddit.py
+++ reddit.py
@@ -1,4 +1,4 @@
-import praw
+import praw,tweepy,sys,time
 
 my_user_agent = "mac:RedditSaleReminder:v1.0 (by /u/bdang24)"
 my_client_id = "uFm6USAofxMHQQ"
@@ -12,13 +12,35 @@ reddit = praw.Reddit(user_agent=my_user_agent,
                      username=my_username,
                      password=my_password)
 
+consumer_key = "oXStSpBeEUS3ayaVay8IxSqzC"
+consumer_secret = "clRKjABnCvmVHaf9iul9CJ2ka9zCwFaLBLTcwPqx5PBVecMWcd"
+access_key = "802490163578318848-kO2FiftQ1VGX3azus58IFpTSQCIYi7e"
+access_secret = "ABJuzgKYoLfiO4tri3tXC4lkcaKCfzFScATYUEzoDm3tX"
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_key, access_secret)
+api = tweepy.API(auth)
+
+filename = open("reddit.txt", "r+")
+
 loop = "yes";
 while loop != "no":
 	user_input = raw_input("Please enter the category you want to search: CPU, GPU, RAM, or SSD ")
 	user_number = int(raw_input("How many results would you like to see? "))
 	search_results = reddit.search(user_input, subreddit="buildapcsales", sort="new", syntax = None, period = "all time", limit = user_number)
+	tweet = raw_input("Would you like to tweet these results individually? Type yes or no. ")
+	filename = open("reddit.txt", "r+")
+	filename.truncate(0)
 	for x in search_results:
-		print x;
+		converted = str(x)
+		filename.write(time.strftime("%I:%M:%S"))
+		filename.write(converted)
+		filename.write("\n")
+	filename.close();
+	if tweet == "yes":
+		file = open("reddit.txt", "r")
+		f = file.readlines()
+		for line in f:
+			api.update_status(status = line)
+		file.close()
 	loop = raw_input("Would you like to make a new search? Type yes or no. ")
 
-	
